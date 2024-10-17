c8a0f0a9a792958a8c68a4c998a209acc620594c
coffee-marios <mark_theo@yahoo.com>
2024-10-17 11:26:12 +0300

    Add an event listener that allows the player to hit targets on the grid

--- script.mjs
+++ script.mjs
@@ -1,19 +1,28 @@
 import { Ship, Gameboard, Player } from "./src/utils.mjs";
 
-const createBoard = (elementId) => {
+const createBoard = (elementId, elementClass, player = "person") => {
   const board_1 = document.getElementById(elementId);
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
       const elem = document.createElement("div");
       const blockId = `${elementId}-[${i},${j}]`;
-      elem.classList.add("board_block_1");
+      elem.classList.add(elementClass);
       elem.id = blockId;
+      const dot = document.createElement("div");
+      elem.appendChild(dot);
+      if (player === "person") {
+        elem.addEventListener("click", () => {
+          dot.classList.add("dot-strike");
+          elem.classList.add("board_block_1-strike");
+        });
+      }
+
       board_1.appendChild(elem);
-      // if (i === 4) console.log(elem.id);
+      //if (i === 4) console.log(elem.id);
     }
   }
 };
-createBoard("board_1");
-createBoard("board_2");
+createBoard("board_1", "board_block_1");
+createBoard("board_2", "board_block_2", "computer");
 
 console.log();
